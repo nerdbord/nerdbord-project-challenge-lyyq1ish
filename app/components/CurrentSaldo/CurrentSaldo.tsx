534970d882c5730561253a475448b2440d97761b
Adrian <klimasa98@gmail.com>
2024-08-03 11:13:16 +0200

    working on spendings page

---
+++ app/components/CurrentSaldo/CurrentSaldo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CurrentSaldo = () => {
+  return (
+    <h3 className="py-8 text-center text-3xl">
+      Saldo: <span>5000 zł</span>
+    </h3>
+  )
+}
+
+export default CurrentSaldo
--- app/components/ReceiptList/ReceiptList.tsx
+++ app/components/ReceiptList/ReceiptList.tsx
@@ -1,26 +1,24 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import {
-  getReceiptsForUser,
-  getReceiptById,
-} from '@/app/actions/receiptActions'
-import styles from './ReceiptList.module.scss'
+import { useRouter } from 'next/navigation'
+import { getReceiptsForUser } from '@/app/actions/receiptActions'
+import Link from 'next/link'
+import { Poppins } from 'next/font/google'
 
 interface Receipt {
   id: string
-  image: string | null
   date: string | null
   shop: string | null
   total: string | null
-  receiptNumber: string | null
   category: string | null
-  description: string | null
 }
+
+const poppins = Poppins({ weight: '300', subsets: ['latin'] })
 
 export default function ReceiptList() {
   const [receipts, setReceipts] = useState<Receipt[]>([])
-  const [selectedReceipt, setSelectedReceipt] = useState<Receipt | null>(null)
+  const router = useRouter()
 
   useEffect(() => {
     async function fetchReceipts() {
@@ -34,52 +32,32 @@ export default function ReceiptList() {
     fetchReceipts()
   }, [])
 
-  const handleReceiptClick = async (receiptId: string) => {
-    try {
-      const receipt = await getReceiptById(receiptId)
-      setSelectedReceipt(receipt)
-    } catch (error) {
-      console.error('Error fetching receipt details:', error)
-    }
-  }
-
-  const handleCloseClick = () => {
-    setSelectedReceipt(null)
+  const handleReceiptClick = (receiptId: string) => {
+    router.push(`/receipt/${receiptId}`)
   }
 
   return (
-    <div className={styles.receiptListContainer}>
-      <div className={styles.receiptGrid}>
-        {receipts.map((receipt, index) => (
-          <div
+    <div className="m-4 rounded-xl bg-[#EEEBEB] p-2">
+      <div className={`flex items-center justify-around ${poppins.className}`}>
+        <h4 className="text-[28px]">Moje wydatki:</h4>
+        <Link className="text-[14px]" href="/">
+          Eksportuj
+        </Link>
+      </div>
+      <ul className="space-y-4">
+        {receipts.map((receipt) => (
+          <li
             key={receipt.id}
-            className={styles.receiptSquare}
+            className="cursor-pointer rounded border p-4 hover:bg-gray-100"
             onClick={() => handleReceiptClick(receipt.id)}
           >
-            <span className={styles.receiptNumber}>{index + 1}</span>
-            {receipt.image && (
-              <img src={receipt.image} alt="Receipt thumbnail" />
-            )}
-          </div>
+            <p className="font-semibold">{receipt.category}</p>
+            <p> {receipt.shop}</p>
+            <p> {receipt.date}</p>
+            <p> {receipt.total}</p>
+          </li>
         ))}
-      </div>
-      {selectedReceipt && (
-        <div className={styles.receiptDetails}>
-          <button className={styles.closeButton} onClick={handleCloseClick}>
-            &times;
-          </button>
-          <h2>Szczegóły paragonu</h2>
-          <p>Data: {selectedReceipt.date}</p>
-          <p>Sklep: {selectedReceipt.shop}</p>
-          <p>Suma: {selectedReceipt.total}</p>
-          <p>Numer paragonu: {selectedReceipt.receiptNumber}</p>
-          <p>Kategoria: {selectedReceipt.category}</p>
-          <p>Opis: {selectedReceipt.description}</p>
-          {selectedReceipt.image && (
-            <img src={selectedReceipt.image} alt="Full receipt" />
-          )}
-        </div>
-      )}
+      </ul>
     </div>
   )
 }
