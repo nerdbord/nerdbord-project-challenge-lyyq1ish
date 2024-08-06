7493ab85c99937789dc5917e55060cc7b42e9b41
Adrian <klimasa98@gmail.com>
2024-08-06 18:18:08 +0200

    deploy

--- app/receipt/[id]/page.tsx
+++ app/receipt/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="min-h-screen bg-[#fff]">
+    <div className="min-h-screen">
       <SingleReceipt />
     </div>
   )
